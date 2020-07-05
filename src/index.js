dc13238879da99d08e6f77384f5d266d9d7baf8d
ksenia <arfloger@gmail.com>
2020-07-05 21:31:52 +0300

    Страницы применение и регионы

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import 'slick-carousel'
+// import 'slick-carousel'
 //import '@fancyapps/fancybox'
 
-import './js/common'
-import './js/lang'
-import './js/select'
-import './js/menu'
-import './js/header'
-import './js/contact-card'
+// import './js/common'
+// import './js/lang'
+// import './js/select'
+// import './js/menu'
+// import './js/header'
+// import './js/contact-card'
 
 
 import './assets/scss/main.scss'
