fcd27a33023367679037c708f0828ea930ed04e7
ksenia <arfloger@gmail.com>
2020-07-09 17:38:43 +0300

    Внутренняя страница новости адаптив

--- src/index.js
+++ src/index.js
@@ -22,4 +22,21 @@ $(`.slider`).slick({
     appendDots: $(`.slider__dots`),
     prevArrow: $(`.slider__prev`),
     nextArrow: $(`.slider__next`),
+    responsive: [{
+        breakpoint: 641,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+        }
+    },
+    {
+        breakpoint: 481,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrow: false,
+            variableWidth: true
+        }
+    }]
+
 })
